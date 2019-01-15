fbffd33a3db141d9eaf13c32ac4fab79f7ddafa1
Johnathan MEUNIER <johnathan2693@gmail.com>
2019-01-15 13:56:11 +0100

    feat(another): envoie de contenu sur une autre route

--- express/server.js
+++ express/server.js
@@ -12,14 +12,19 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({
-  route: req.originalUrl
-}));
+router.get('/another', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<h1>Another Route</h1>');
+  res.end();
+});
 router.post('/', (req, res) => res.json({
   postBody: req.body
 }));
 
 app.use(bodyParser.json());
+app.use('', router); // path must route to lambda
 app.use('/.netlify/functions/server', router); // path must route to lambda
 
 module.exports = app;
